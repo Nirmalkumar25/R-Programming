90f744b192f3ffd196019fdecd946de7497fa5e6
Nirmalkumar S <160230112+NirmalAlgo@users.noreply.github.com>
2024-07-28 12:41:26 +0530

    List concept

---
+++ list.R
@@ -0,0 +1,13 @@
+# List
+# List is a generic vector which contain object of different base types
+# there is no coercion in list and no loss of functionality
+# list() - function to create a dataframe
+
+list.1 <- list(x=c("a","b","c"),
+            y=c(TRUE,FALSE,F,T,F),
+            z=c(45,34,76,567))
+list.1
+
+
+list.2 <- list(1,23,4)
+list.2
